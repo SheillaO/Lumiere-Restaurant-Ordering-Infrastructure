549818c3515ce9a1dadf7039b40f28fd12109ee1
SheillaO <sheilla.alusa@strathmore.edu>
2026-04-29 15:57:40 +0300

    Update data.js

--- data.js
+++ data.js
@@ -22,4 +22,26 @@ export const menuArray = [
         reviews: 98,
         calories: 400
     },
-    
+     {
+        name: "Velouté of Roasted Heirloom Carrots",
+        category: "Starters",
+        ingredients: ["slow-roasted heirloom carrots", "coriander cream", "parsnip crisps", "fresh cilantro oil"],
+        id: 2,
+        price: 22,
+        image: "images/soup.jpg",
+        rating: 4.6,
+        reviews: 65,
+        calories: 250
+    },
+     // 🍔 Mains
+    {
+        name: "Wagyu Beef Burger with Truffle Emulsion",
+        category: "Main",
+        ingredients: ["A5 wagyu beef patty", "aged cheddar", "butter lettuce", "black truffle aioli", "brioche bun"],
+        id: 3,
+        price: 60,
+        image: "images/burger.jpg",
+        rating: 4.9,
+        reviews: 89,
+        calories: 920
+    },
